1d0608460c1bf2c60b771a65fb17db91e35d9660
Yuto Takano <moa17stock@gmail.com>
2020-10-11 21:38:51 +0100

    Add keybindings to move to next/previous post

--- PiazzaHistoryKeybindings.user.js
+++ PiazzaHistoryKeybindings.user.js
@@ -11,6 +11,8 @@
 
 // Avoid conflicts
 this.$ = this.jQuery = jQuery.noConflict(true);
+// make current post item focus()-able
+$("[data-pats=current_post]").attr("tabindex", 1);
 
 function moveHistorySlider(increment) {
     // `P` is a Piazza internal variable
@@ -19,6 +21,36 @@ function moveHistorySlider(increment) {
     if(newVal < P.history_slider.slider.min) newVal = P.history_slider.slider.min;
     if(newVal > P.history_slider.slider.max) newVal = P.history_slider.slider.max;
     P.history_slider.slider.element.slider("setValue", newVal).trigger("slide");
+}
+
+function prevPost() {
+    // There is a P.feed.feedItemClick(event, this) attached to each post's feed item
+    // Easiest solution to trigger this is to just utilise jQuery's click() function.
+    var curr = $("[data-pats=post_group_item].selected");
+    // attempt to find previous post in same group
+    var prev_post = curr.prev("[data-pats=post_group_item]");
+    if (prev_post.length != 0) {
+        prev_post.click();
+    } else {
+        // attempt to find previous post in previous group
+        var prev_group = curr.closest("[data-pats=post_group]").prev("[data-pats=post_group]");
+        if (prev_group.length != 0) prev_group.find("[data-pats=post_group_item]:last")[0].click();
+    }
+    // return focus to the current post so PgUp/PgDn keys work
+    $("[data-pats=current_post]").attr("tabindex", 1).focus();
+}
+
+function nextPost() {
+    var curr = $("[data-pats=post_group_item].selected");
+    var next_post = curr.next("[data-pats=post_group_item]");
+    if (next_post.length != 0) {
+        next_post.click();
+    } else {
+        var next_group = curr.closest("[data-pats=post_group]").next("[data-pats=post_group]");
+        if (next_group.length != 0) next_group.find("[data-pats=post_group_item]")[0].click();
+    }
+    // return focus to the current post so PgUp/PgDn keys work
+    $("[data-pats=current_post]").attr("tabindex", 1).focus();
 }
 
 $(document).ready(function () {
@@ -34,10 +66,15 @@ $(document).ready(function () {
             case 37:
                 moveHistorySlider(-1);
                 break;
+            case 38:
+                prevPost();
+                break;
             case 39:
                 moveHistorySlider(1);
                 break;
-
+            case 40:
+                nextPost();
+                break;
             default: return;
         }
         e.preventDefault();
